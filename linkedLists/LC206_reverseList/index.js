b22e2c99ee44a5de2e69ac0d6f061955228b4923
ramiro <rhuayller@agetic.gob.bo>
2021-02-10 14:16:07 -0400

    se termino linked list

--- linkedLists/LC206_reverseList/index.js
+++ linkedLists/LC206_reverseList/index.js
@@ -1,3 +1,15 @@
-function reverseList(head) {}
+function reverseList(head) {
+    let preview = null
+    let current = head
+
+    while (current) {
+        let temp = current.next
+        current.next = preview
+        preview = current
+        current = temp
+
+    }
+    return preview
+}
 
 module.exports = reverseList;
