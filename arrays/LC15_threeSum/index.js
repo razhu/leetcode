66964b86a3b3ea4c44017127f33518d5ab86b545
ramiro <rhuayller@agetic.gob.bo>
2021-02-11 15:59:56 -0400

    ok

--- arrays/LC15_threeSum/index.js
+++ arrays/LC15_threeSum/index.js
@@ -1,3 +1,22 @@
-function threeSum(nums) {}
+function threeSum(nums) {
+    nums.sort((a, b) => a - b)
+    let right = nums.length - 1
+    let resultado = []
+    for (let i = 0; i < nums.length; i++) {
+        let left = 1
+        let partial = []
+        for (let j = left; j <= nums.length - right; j++) {
+            if (nums[i] + nums[j] + nums[right] === 0) {
+                partial.push(nums[i])
+                partial.push(nums[j])
+                partial.push(nums[right])
+                resultado.push(partial)
+            }
+        }
+        left++
+        right--
+    }
+    return resultado;
+}
 
 module.exports = threeSum;
