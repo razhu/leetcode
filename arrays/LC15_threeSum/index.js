b800b9f520db8ad2b61ab74584a51f1164a08584
ramiro <ramihertz@gmail.com>
2021-02-15 09:57:08 -0400

    ok

--- arrays/LC15_threeSum/index.js
+++ arrays/LC15_threeSum/index.js
@@ -2,11 +2,11 @@ function threeSum(nums) {
     nums.sort((a, b) => a - b)
     let right = nums.length - 1
     let resultado = []
+    let left = 1
+    let partial = []
     for (let i = 0; i < nums.length; i++) {
-        let left = 1
-        let partial = []
-        for (let j = left; j <= nums.length - right; j++) {
-            if (nums[i] + nums[j] + nums[right] === 0) {
+        for (let j = left; j <= right; j++) {
+            if ((nums[i] + nums[j] + nums[right]) === 0) {
                 partial.push(nums[i])
                 partial.push(nums[j])
                 partial.push(nums[right])
@@ -20,3 +20,4 @@ function threeSum(nums) {
 }
 
 module.exports = threeSum;
+ 
